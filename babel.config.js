7b5c2781e72c822f32366ec9aa9e5cf31f47c1c3
Clayton Prebelli <clayton.prebelli.act@gmail.com>
2026-05-24 22:57:47 -0300

    Fix: use ubuntu-22.04 runner, babel plugins for Hermes compat

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { lazyImports: true }]],
+    presets: ['babel-preset-expo'],
     plugins: [
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-property-in-object',
     ],
   };
 };
